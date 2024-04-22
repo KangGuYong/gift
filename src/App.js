1786c2e08aa73f484323b04c74c32165c6481bdf
kang <kgy00100431@gmail.com>
2024-04-22 21:20:48 +0900

    2 commit

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ import image15 from './Photo/또래방.jpg';
 import image16 from './Photo/시험전.jpg';
 import image17 from './Photo/편맥.jpg';
 const photos = [
-  { id: 1, src: image1, description: '2024/03/27(화요일)\n보금자리 첫 활동' },
+  { id: 1, src: image1, description: '2024/03/27(화요일)\n보금자리 첫 활동\n\n"누나 처음 만난 날"' },
   { id: 2, src: image2, description: '2024/04/04(목요일)\n보금자리 2번째 활동' },
-  { id: 3, src: image3, description: '2024/04/09(화요일)\n기숙사 파티 동방 같이 가기\n 위해서 기다리는 구용이' },
+  { id: 3, src: image3, description: '2024/04/09(화요일)\n기숙사 파티 동방 같이 가기\n 위해서 기다리는 구용이\n\n"나만 빨리 나와ㅋㅋㅋㅋ"' },
   { id: 4, src: image4, description: '2024/04/09(화요일)\n보금자리 3번째 활동' },
-  { id: 6, src: image5, description: '2024/04/09(화요일)-2024/04/10(수요일)\n회식' },
-  { id: 5, src: image6, description: '2024/04/09(화요일)\n기숙사 벚꽃 사진' },
-  { id: 7, src: image7, description: '2024/04/14(일요일)\n하람 누나가 물가져가고 두고간 코콜릿' },
-  { id: 8, src: image8, description: '2024/04/15(월요일)\n예비군 가서 하람누나가 준 초콜릿 잘 먹었어' },
-  { id: 9, src: image9, description: '2024/04/18(목요일)\n 노래방 갔다가 하이볼 마시러 할맥' },
-  { id: 10, src: image10, description: '2024/04/19(금요일)\n 노래방->할맥->기숙사->인생네컷\n연습샷' },
+  { id: 6, src: image5, description: '2024/04/09(화요일)-2024/04/10(수요일)\n회식\n\n"이때가 누가 처음 취한 날???"' },
+  { id: 5, src: image6, description: '2024/04/09(화요일)\n기숙사 벚꽃 사진\n\n"나만 이상해ㅋㅋㅋㅋ 봄,여름,가을,겨울 사진고 이으면 멋 있을 듯?"' },
+  { id: 7, src: image7, description: '2024/04/14(일요일)\n하람 누나가 물가져가고 두고간 초콜릿' },
+  { id: 8, src: image8, description: '2024/04/15(월요일)\n예비군 가서 하람누나가 준 초콜릿\n\n"우마이!!!"' },
+  { id: 9, src: image9, description: '2024/04/18(목요일)\n 노래방 갔다가 하이볼 마시러 할맥\n\n"하이볼 너무 맛있어 시험 끝나고 또\n 마시러 가자"' },
+  { id: 10, src: image10, description: '2024/04/19(금요일)\n 노래방->할맥->기숙사->인생네컷\n연습샷\n\n"간지 촬촬"' },
   { id: 11, src: image11, description: '2024/04/19(금요일)\n인생네컷 포즈 원본' },
-  { id: 12, src: image12, description: '2024/04/19(금요일)\nㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ' },
-  { id: 13, src: image13, description: '2024/04/19(금요일)\n진짜 전부다 너무 잘 나왔어' },
+  { id: 12, src: image12, description: '2024/04/19(금요일)\n인생네컷\n\n"ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"' },
+  { id: 13, src: image13, description: '2024/04/19(금요일)\n인생팔컷\n\n"8장 전부 너무 잘 나왔어ㅋㅍㅋㅍㅋㅍㅋ"' },
   { id: 14, src: image14, description: '2024/04/19(금요일)\n사샷' },
-  { id: 15, src: image15, description: '2024/04/19(금요일)\n노래방->할맥->기숙사->인생네컷->\n노래방'},
-  { id: 16, src: image17, description: '2024/04/19(금요일)\n노래방->할맥->기숙사->인생네컷->\n노래방->편맥때 본\n고양이' },
-  { id: 17, src: image16, description: '2024/04/19(금요일)\n신나게 놀고 또 낭만 찾기ㅋㅋㅋㅋ' },
+  { id: 15, src: image15, description: '2024/04/19(금요일)\n노래방->할맥->기숙사->인생네컷->\n노래방\n\n"?????"'},
+  { id: 16, src: image17, description: '2024/04/19(금요일)\n노래방->할맥->기숙사->인생네컷->\n노래방->편맥때 본\n고양이\n\n"고먐미"' },
+  { id: 17, src: image16, description: '2024/04/19(금요일)\n돗자리 피고 낭만 찾기\n\n"이날 놀지 말았어야했어"' },
 ];
 function Login({ onSuccess }) {
   const [username, setUsername] = useState('');
@@ -51,24 +51,26 @@ function Login({ onSuccess }) {
 
   return (
     <div className="login-container">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>이름:</label>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="input-group">
           <input
             type="text"
+            placeholder="아이디"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            required
           />
         </div>
-        <div>
-          <label>생일 8자리:</label>
+        <div className="input-group">
           <input
             type="password"
+            placeholder="비밀번호"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
-        <button type="submit">로그인</button>
+        <button type="submit" className="login-button">로그인</button>
       </form>
     </div>
   );
